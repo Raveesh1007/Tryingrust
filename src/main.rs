22c953bd5ba156a97031fa351a6512848dbde1da
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-08-21 14:24:51 +0530

    Second Commit. Learnt about stacks & heap function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 fn main(){
-    let greeting  = String::from("Hello World");
-    print!("{}", greeting);
-
-    let char1 = greeting.chars().nth(1000);
-
-    match char1 {
-        Some(c) => print!("{}", c),
-        None => print!("No character at index 1000"),
-
-    }
-
-    print!("char1 :{:?}", char1);
-
-
+    stack_fn();
+    heap_fn();
+    update_fn();
 }
+
+
+fn stack_fn() {
+    let x = 5;
+    let y = 4;
+    let c = x + y ;
+    println!("Stack Function: The sum of {} and {} is {}", x, y, c);
+}
+
+fn heap_fn() {
+    let s1 = String::from("Hello,");
+    let s2 = String::from("World!");
+    let combined = format!("{} {}", s1, s2);
+    println!("Heap Function: {}", combined);
+}
+
+fn update_fn() {
+    let mut s= String::from("Initial String And ");
+     println!("Before Update! {}", s);
+     println!("Capacity {}, Length{}, Pointer{:p}", s.capacity(), s.len(), s.as_ptr()); 
+
+
+    s.push_str("Good Morning!");
+    println!("After Update! {}", s);
+    println!("Capacity {}, Length{}, Pointer{:p}", s.capacity(), s.len(), s.as_ptr());
+}
+
