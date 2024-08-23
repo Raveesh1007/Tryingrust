4e713693bed8151208a931e574ae5ae02781c3a7
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-08-23 21:33:44 +0530

    ownership commit

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,17 @@
-fn main(){
-    stack_fn();
-    heap_fn();
-    update_fn();
+fn main (){
+    let x = String::from ("Hello World");
+
+    takes_ownership (x);
+
+    let s = 5;
+
+    makes_copy(s);
 }
 
-
-fn stack_fn() {
-    let x = 5;
-    let y = 4;
-    let c = x + y ;
-    println!("Stack Function: The sum of {} and {} is {}", x, y, c);
+fn takes_ownership(x: String){
+    println!("{}", x);
 }
 
-fn heap_fn() {
-    let s1 = String::from("Hello,");
-    let s2 = String::from("World!");
-    let combined = format!("{} {}", s1, s2);
-    println!("Heap Function: {}", combined);
+fn makes_copy(s: i32){
+    println!("{}", s);
 }
-
-fn update_fn() {
-    let mut s= String::from("Initial String And ");
-     println!("Before Update! {}", s);
-     println!("Capacity {}, Length{}, Pointer{:p}", s.capacity(), s.len(), s.as_ptr()); 
-
-
-    s.push_str("Good Morning!");
-    println!("After Update! {}", s);
-    println!("Capacity {}, Length{}, Pointer{:p}", s.capacity(), s.len(), s.as_ptr());
-}
-
