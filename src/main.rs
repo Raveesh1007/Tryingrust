f3478abcb9de6972eb3f942ceb4e1311d603b3df
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-08-26 18:24:52 +0530

    first shit commit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-enum Coin(){
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
 
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
 }
 
-fn value_in_cents(coin: Coin) -> u8{
-    match coin{
-        Coin::Penny => {
-            println!("Lucky Penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
-    }
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
