764618e4ac3f2b3f97dc68f9798b9609af88485f
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-09-11 20:32:42 +0530

    iterators in rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main(){
-    let v1 = vec![1,2,3];
+    let v1 = vec![1, 2, 3];
+
     let v1_iter = v1.iter();
 
-    for val in v1_iter{
-        println!("Got: {val}")
-    } 
-
+    let sum : i32 = v1_iter.sum();
+    println!("sum is {}", sum);
 
 }
