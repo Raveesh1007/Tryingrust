9b76012d8e78f32e4ecf46950f5751d8089f2aa3
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-09-14 22:33:50 +0530

    wow what a  comiit

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,7 @@ struct User {
 }
 
 impl Summary for User {
-    fn summarize (&self) -> String{
-        return format!("Name: {}, Age: {},", self.name, self.age); 
-    }
-}
+
 
 fn main (){
     let user = User {
