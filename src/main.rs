6452f5ea16f5682679ffb246888d251d3b3e2a30
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-09-19 20:45:46 +0530

    ok ok i did the chenges

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,10 @@ impl Summary for User {
     }
 }
 
+fn main (){
+    let user = User {
+        name : String::from("Raveesh"),
+        age : 25,
+    };
+    println!("{}", user.summarize());
+}
